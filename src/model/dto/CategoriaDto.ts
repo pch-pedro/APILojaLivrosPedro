63fff2fe0049ae44f3308a2e375b62f4bdf925e6
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-12-09 00:06:22 -0300

    Teste

--- src/model/dto/CategoriaDto.ts
+++ src/model/dto/CategoriaDto.ts
@@ -1,26 +1,10 @@
-export class CategoriaModel{
+export class CategoriaDto{
 
     id?: number;
     nome: string;
 
     constructor(nome: string, id?: number){
         this.id = id;
-        this.nome = nome;
-    }
-
-    //GETTERS
-
-    getId(): number | undefined{
-        return this.id;
-    }
-
-    getNome(): string{
-        return this.nome;
-    }
-
-    //SETTERS
-
-    setNome(nome: string): void{
         this.nome = nome;
     }
 }
