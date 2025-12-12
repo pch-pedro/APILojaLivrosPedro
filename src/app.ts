b0923fb6cf9ea4d0335c89ff49fa4c7b218665da
Pedro Pacheco <sp.pedro.pacheco23.3@gmail.com>
2025-12-12 08:56:17 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,6 @@ app.use(cors({
     credentials: true 
 }));
 
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-    credentials: false
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
