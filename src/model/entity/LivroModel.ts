9e1b8ffb30ca99ed841536d1712dd596997753bb
Pedro Pacheco <sp.pedro.pacheco23.3@gmail.com>
2025-12-12 10:03:21 -0300

    Update LivroModel.ts

--- src/model/entity/LivroModel.ts
+++ src/model/entity/LivroModel.ts
@@ -8,12 +8,12 @@ export class LivroModel {
     preco: number;
     estoque: number;
     sinopse: string;
-    image_url: string;
+    imageURL: string;
     editora: string;
     data_publicacao: Date;
     promocao? : boolean;
 
-    constructor(categoria_id: number, titulo: string, autor: string, isbn: string, preco: number, estoque: number, sinopse: string, image_url: string, editora: string, data_publicacao: Date, promocao?: boolean, id?: number) {
+    constructor(categoria_id: number, titulo: string, autor: string, isbn: string, preco: number, estoque: number, sinopse: string, imageURL: string, editora: string, data_publicacao: Date, promocao?: boolean, id?: number) {
         this.id = id;
         this.categoria_id = categoria_id;
         this.titulo = titulo;
@@ -22,7 +22,7 @@ export class LivroModel {
         this.preco = preco;
         this.estoque = estoque;
         this.sinopse = sinopse;
-        this.image_url = image_url;
+        this.imageURL = imageURL;
         this.editora = editora;
         this.data_publicacao = data_publicacao;
         this.promocao = promocao ?? false;
