20f404021fa7761e9b606762fc58eb956b2aafa4
serrabruna <brunaibserra@hotmail.com>
2025-12-11 21:26:18 -0300

    build

--- dist/app.js
+++ dist/app.js
@@ -17,8 +17,10 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false
 }));
+app.options("*", (0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 (0, routes_js_1.RegisterRoutes)(app);
--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,11 @@ const app = express();
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false
 }));
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
