6adf549ebb6806b4a89e448791f10b1cd9e7a034
pch.pedro <sp.pedro.pacheco23.3@gmail.com>
2025-12-12 02:25:42 -0300

    Swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,12 +1,12 @@
-import swaggerUi from 'swagger-ui-express';
-import type { Express } from 'express';
-import path from 'path';
-import fs from 'fs';
+import swaggerUi from "swagger-ui-express";
+import type { Express } from "express";
+import path from "path";
+import fs from "fs";
 
 export const setupSwagger = (app: Express) => {
-    const swaggerPath = path.join(__dirname, '../swagger.json'); 
-    const swaggerSpec = JSON.parse(fs.readFileSync(swaggerPath, 'utf8'));
+    const swaggerPath = path.join(__dirname, "../swagger/swagger.json");
+    const swaggerSpec = JSON.parse(fs.readFileSync(swaggerPath, "utf8"));
 
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
